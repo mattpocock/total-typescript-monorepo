1d37701f00063f8bf96ff6392af298ec99e44efa
Matt Pocock <mattpocockvoice@gmail.com>
2024-06-04 08:22:14 +0100

    Made the email cli work via glob

--- apps/twoslash-to-email/src/bin.ts
+++ apps/twoslash-to-email/src/bin.ts
@@ -3,46 +3,55 @@
 import {
   exitProcessWithError,
   getActiveEditorFilePath,
+  type AbsolutePath,
   type AnyPath,
   type RelativePath,
 } from "@total-typescript/shared";
 import { applyShiki } from "@total-typescript/twoslash-preview-shared";
 import { Command } from "commander";
-import { readFileSync } from "fs";
+import { readFile, writeFile } from "node:fs/promises";
 import path from "path";
+import fg from "fast-glob";
 
 const program = new Command();
 
 program.version("0.0.1");
 
-program.argument("[file-path]").action(async (filePath?: RelativePath) => {
-  let resolvedFilePath: AnyPath;
-
-  if (!filePath) {
-    const activeEditorFilePath = await getActiveEditorFilePath();
-
-    if (!activeEditorFilePath) {
-      exitProcessWithError("No file path provided and no active editor found");
-    }
-
-    resolvedFilePath = activeEditorFilePath;
-  } else {
-    resolvedFilePath = filePath;
+const wrappedApplyShiki = async (
+  filePath: AbsolutePath,
+  fileContents: string,
+) => {
+  try {
+    return await applyShiki(fileContents);
+  } catch (error: any) {
+    console.error(filePath);
+    console.error(error.title);
+    console.error(error.description);
+    console.error(error.recommendation);
+    exitProcessWithError("Error applying shiki");
   }
+};
 
-  if (!resolvedFilePath.endsWith(".md")) {
-    exitProcessWithError("File must be a markdown file.");
+program.argument("<glob>").action(async (globString: string) => {
+  const filePaths = (await fg(globString)).map((relativePath) => {
+    return path.resolve(process.cwd(), relativePath) as AbsolutePath;
+  });
+
+  for (const filePath of filePaths) {
+    const fileContents = await readFile(filePath, "utf-8");
+
+    const result = await wrappedApplyShiki(filePath, fileContents);
+
+    const cssLocation = path.resolve(import.meta.dirname, "output.css");
+
+    const css = await readFile(cssLocation, "utf-8");
+
+    const output = [`<style>`, css, `</style>`, result.html].join("\n");
+
+    const outputFilename = filePath + ".email.html";
+
+    writeFile(outputFilename, output, "utf-8");
   }
-
-  const fileContents = readFileSync(resolvedFilePath, "utf-8");
-
-  const result = await applyShiki(fileContents);
-
-  const cssLocation = path.resolve(import.meta.dirname, "output.css");
-
-  const css = readFileSync(cssLocation, "utf-8");
-
-  console.log([`<style>`, css, `</style>`, result.html].join("\n"));
 });
 
 program.parse(process.argv);
