4e2c4b8ce12416cd4900bcb4a3fed24888a5776c
Matt Pocock <mattpocockvoice@gmail.com>
2024-07-18 14:43:22 +0100

    Improved the way meta is extracted

--- apps/remotion-code-hike/src/meta.ts
+++ apps/remotion-code-hike/src/meta.ts
@@ -1,10 +1,44 @@
-import _meta from "./meta.local.json";
+import { z } from "zod";
 
-export const meta = _meta as {
-  width?: number;
-  height?: number;
-  durations?: number[];
-  music?: boolean;
-  musicFullVolume?: boolean;
-  slideDuration?: number;
-};
+export type Meta = z.output<typeof metaSchema>;
+
+const metaSchema = z.object({
+  width: z
+    .number()
+    .optional()
+    .describe("Width of the video."),
+  height: z
+    .number()
+    .optional()
+    .describe("Height of the video."),
+  durations: z
+    .array(z.number())
+    .optional()
+    .describe("Durations in ms of each slide."),
+  music: z
+    .boolean()
+    .optional()
+    .describe(
+      "Whether to play music. Overridden by musicFullVolume if present.",
+    ),
+  musicFullVolume: z
+    .boolean()
+    .optional()
+    .describe("Whether to play music at full volume."),
+  slideDuration: z
+    .number()
+    .optional()
+    .describe(
+      "Duration of each slide. Overridden by durations if present.",
+    ),
+});
+
+export let meta: Meta;
+
+try {
+  meta = metaSchema.parse(
+    require("./meta.local.json"),
+  );
+} catch (e) {
+  meta = {};
+}
