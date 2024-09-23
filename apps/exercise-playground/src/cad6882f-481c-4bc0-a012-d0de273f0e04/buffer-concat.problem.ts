cd65343e4c1c585d9d86c3dfaadb5e1d3d86d327
Matt Pocock <mattpocockvoice@gmail.com>
2024-09-23 14:06:25 +0100

    Buff ting

---
+++ apps/exercise-playground/src/cad6882f-481c-4bc0-a012-d0de273f0e04/buffer-concat.problem.ts
@@ -0,0 +1,11 @@
+// http://localhost:3004/courses/exercises/cad6882f-481c-4bc0-a012-d0de273f0e04/edit
+
+import { expect, it } from "vitest";
+
+const buffer = Buffer.from([0, 1, 2]);
+
+const newBuffer = null; // Replace this null with code!
+
+it("Should have a length of 5", () => {
+  expect(newBuffer.length).toBe(5);
+});
