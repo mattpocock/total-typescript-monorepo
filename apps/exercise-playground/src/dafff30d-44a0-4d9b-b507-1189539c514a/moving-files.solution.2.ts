f1869fa1470c7705fdbd048619fc2d7ce67c1611
Matt Pocock <mattpocockvoice@gmail.com>
2024-09-24 12:36:34 +0100

    Added moving files exercise

---
+++ apps/exercise-playground/src/dafff30d-44a0-4d9b-b507-1189539c514a/moving-files.solution.2.ts
@@ -0,0 +1,46 @@
+// http://localhost:3004/courses/exercises/dafff30d-44a0-4d9b-b507-1189539c514a/edit
+
+import { expect, it } from "vitest";
+
+import fs from "node:fs/promises";
+
+const moveFileOrDirectory = async (source: string, destination: string) => {
+  await fs.rename(source, destination);
+};
+
+it("Should move a file", async () => {
+  const path = await import("path");
+
+  const source = path.join(import.meta.dirname, "./input.txt");
+  const destination = path.join(import.meta.dirname, "./output.txt");
+
+  // Set up the test
+  const { rm, writeFile } = await import("fs/promises");
+  await writeFile(source, "Hello, world!");
+  await rm(destination, { force: true, recursive: true });
+
+  await moveFileOrDirectory(source, destination);
+
+  // Expect the destination file to exist
+  const { stat } = await import("fs/promises");
+  await expect(stat(destination)).resolves.toBeTruthy();
+});
+
+it("Should move a directory", async () => {
+  const path = await import("path");
+
+  const source = path.join(import.meta.dirname, "./input-dir");
+  const destination = path.join(import.meta.dirname, "./output-dir");
+
+  // Set up the test
+  const { rm, mkdir, writeFile } = await import("fs/promises");
+  await mkdir(source, { recursive: true });
+  await writeFile(path.join(source, "file.txt"), "Hello, world!");
+  await rm(destination, { recursive: true, force: true });
+
+  await moveFileOrDirectory(source, destination);
+
+  // Expect the destination directory to exist
+  const { stat } = await import("fs/promises");
+  await expect(stat(destination)).resolves.toBeTruthy();
+});
