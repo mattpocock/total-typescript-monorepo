35ab7ce5c6cbc5204482bc5ed2bfc7cd1100db8b
Matt Pocock <mattpocockvoice@gmail.com>
2024-09-25 11:24:13 +0100

    Removed transcribeAudio

--- packages/ffmpeg/src/index.ts
+++ packages/ffmpeg/src/index.ts
@@ -2,5 +2,4 @@ export * from "./commands.js";
 export * from "./constants.js";
 export * from "./getFPS.js";
 export * from "./getVideoDuration.js";
-export * from "./transcribeAudio.js";
 export * from "./normalizeAudio.js";
