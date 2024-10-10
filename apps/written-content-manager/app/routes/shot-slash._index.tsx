ed97d1086e9839d1f03a79ea6d4f2227795c1b4d
Matt Pocock <mattpocockvoice@gmail.com>
2024-10-10 09:32:08 +0100

    Made shotslash prettier

--- apps/written-content-manager/app/routes/shot-slash._index.tsx
+++ apps/written-content-manager/app/routes/shot-slash._index.tsx
@@ -10,6 +10,7 @@ import {
   type HTMLRendererSearchParams,
   type RenderType,
 } from "@total-typescript/twoslash-shared";
+import { PageContent, TitleArea } from "~/components";
 import { useSubscribeToSocket } from "~/use-subscribe-to-socket";
 
 export const meta: MetaFunction = () => {
@@ -73,76 +74,98 @@ export default function Index() {
     (params.get("renderType") as RenderType) ?? RENDER_TYPES.simpleNoBorder;
 
   if (data.status === "waiting") {
-    return <p>Waiting for snippet...</p>;
+    return (
+      <Wrapper>
+        <p>Waiting for snippet...</p>
+      </Wrapper>
+    );
   }
 
   return (
-    <div className="my-4 mb-8 space-y-8">
-      <select
-        value={renderType}
-        onChange={(e) => {
-          setSearchParams(
-            (prev) => {
-              prev.set("renderType", e.target.value);
-              return prev;
-            },
-            {
-              preventScrollReset: true,
-            }
-          );
-        }}
-        className="p-2 px-4 rounded-md bg-gray-100 appearance-none"
-      >
-        {Object.entries(RENDER_TYPES).map(([key, value]) => {
-          if (key === "error") return null;
-          return (
-            <option key={key} value={value}>
+    <Wrapper>
+      <div className="space-y-8 max-w-3xl">
+        <select
+          value={renderType}
+          onChange={(e) => {
+            setSearchParams(
+              (prev) => {
+                prev.set("renderType", e.target.value);
+                return prev;
+              },
               {
-                RENDER_TYPE_HUMAN_READABLE_NAMES[
-                  key as keyof typeof RENDER_TYPES
-                ]
+                preventScrollReset: true,
               }
-            </option>
-          );
-        })}
-      </select>
-      {(() => {
-        switch (renderType) {
-          case RENDER_TYPES.allBasicWithBorder:
-          case RENDER_TYPES.allSquareWithBorder:
-            return (
-              <img
-                src={`http://localhost:${IMAGE_SERVER_PORT}/render-from-uri?${new URLSearchParams(
-                  {
-                    uri: data.uri,
-                    mode: renderType,
-                    cacheBuster: data.fileContentsHash,
-                  } satisfies HTMLRendererSearchParams
-                )}`}
-                className="width-96  rounded-lg"
-              />
             );
-          case RENDER_TYPES.basicWithBorder:
-          case RENDER_TYPES.simpleNoBorder:
-            return data.snippetHashes.map((hash, index) => {
+          }}
+          className="p-2 px-4 rounded-md bg-gray-100 appearance-none"
+        >
+          {Object.entries(RENDER_TYPES).map(([key, value]) => {
+            if (key === "error") return null;
+            return (
+              <option key={key} value={value}>
+                {
+                  RENDER_TYPE_HUMAN_READABLE_NAMES[
+                    key as keyof typeof RENDER_TYPES
+                  ]
+                }
+              </option>
+            );
+          })}
+        </select>
+        {(() => {
+          switch (renderType) {
+            case RENDER_TYPES.allBasicWithBorder:
+            case RENDER_TYPES.allSquareWithBorder:
               return (
                 <img
-                  key={index}
                   src={`http://localhost:${IMAGE_SERVER_PORT}/render-from-uri?${new URLSearchParams(
                     {
                       uri: data.uri,
                       mode: renderType,
-                      snippetIndex: String(index),
-                      cacheBuster: hash,
+                      cacheBuster: data.fileContentsHash,
                     } satisfies HTMLRendererSearchParams
                   )}`}
                   className="width-96  rounded-lg"
                 />
               );
-            });
-        }
-        return null;
-      })()}
-    </div>
+            case RENDER_TYPES.basicWithBorder:
+            case RENDER_TYPES.simpleNoBorder:
+              return data.snippetHashes.map((hash, index) => {
+                return (
+                  <img
+                    key={index}
+                    src={`http://localhost:${IMAGE_SERVER_PORT}/render-from-uri?${new URLSearchParams(
+                      {
+                        uri: data.uri,
+                        mode: renderType,
+                        snippetIndex: String(index),
+                        cacheBuster: hash,
+                      } satisfies HTMLRendererSearchParams
+                    )}`}
+                    className="width-96  rounded-lg"
+                  />
+                );
+              });
+          }
+          return null;
+        })()}
+      </div>
+    </Wrapper>
   );
 }
+
+export const Wrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <PageContent>
+      <TitleArea
+        title="ShotSlash"
+        underTitle={
+          <p className="text-gray-600">
+            Use Twoslash to create awesome screenshots for your posts.
+          </p>
+        }
+      ></TitleArea>
+      {children}
+    </PageContent>
+  );
+};
