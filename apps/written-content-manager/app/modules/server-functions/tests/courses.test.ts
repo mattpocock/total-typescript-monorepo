b88c178aaa100a83947cdb69a0abebb2ca907c38
Matt Pocock <mattpocockvoice@gmail.com>
2024-10-13 14:58:24 +0100

    Removed dead import

--- apps/written-content-manager/app/modules/server-functions/tests/courses.test.ts
+++ apps/written-content-manager/app/modules/server-functions/tests/courses.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it, vitest } from "vitest";
+import { describe, expect, it } from "vitest";
 import { p } from "../../../db";
 import { serverFunctions } from "../server-functions";
 
