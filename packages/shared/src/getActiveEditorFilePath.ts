81b4e416a120ae0207af41aa556179cc7e7bb0e4
Matt Pocock <mattpocockvoice@gmail.com>
2024-10-31 08:45:19 +0000

    Refactored getActiveEditorFilePath into a Result

--- packages/shared/src/getActiveEditorFilePath.ts
+++ packages/shared/src/getActiveEditorFilePath.ts
@@ -1,3 +1,6 @@
-export const getActiveEditorFilePath = async () => {
-  throw new Error("getActiveEditorFilePath is no longer working");
+import { errAsync, ResultAsync } from "neverthrow";
+import type { AbsolutePath } from "./types.js";
+
+export const getActiveEditorFilePath = (): ResultAsync<AbsolutePath, Error> => {
+  return errAsync(new Error("getActiveEditorFilePath is no longer working"));
 };
